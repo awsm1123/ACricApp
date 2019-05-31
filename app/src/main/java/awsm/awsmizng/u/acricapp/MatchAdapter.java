b0870b769aa5ee6a7912c497a93219a540d78013
awsm1123 <jaayushmax@gmail.com>
2019-05-31 22:23:24 +0530

    polish cardview ui

--- app/src/main/java/awsm/awsmizng/u/acricapp/MatchAdapter.java
+++ app/src/main/java/awsm/awsmizng/u/acricapp/MatchAdapter.java
@@ -1,5 +1,6 @@
 package awsm.awsmizng.u.acricapp;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Random;
 
 public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.CustomViewHolder> {
     private List<RetroMatches> matchList;
@@ -19,11 +21,13 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.CustomViewHo
     class CustomViewHolder extends RecyclerView.ViewHolder{
         public final View myView;
         TextView tvMatch;
+        View viewLeft, viewRight;
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             myView=itemView;
             tvMatch=myView.findViewById(R.id.tvTeam1);
-            //TODO findviewby id for textview from mmy view
+            viewLeft=myView.findViewById(R.id.viewLeft);
+            viewRight=myView.findViewById(R.id.viewRight);
         }
     }
     @NonNull
@@ -39,8 +43,15 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.CustomViewHo
     public void onBindViewHolder(@NonNull CustomViewHolder viewHolder, int i) {
 
         RetroMatches retroMatches = matchList.get(i);
-        viewHolder.tvMatch.setText(retroMatches.getTeam1());
-        //TODO set proper details here
+        viewHolder.tvMatch.setText(retroMatches.getTeam1() + " Vs " +retroMatches.getTeam2());
+
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        viewHolder.viewLeft.setBackgroundColor(color);
+
+        Random rnd2 = new Random();
+        int color2 = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        viewHolder.viewRight.setBackgroundColor(color2);
     }
 
     @Override
